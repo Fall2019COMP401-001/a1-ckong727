03933c5005ce27638bdbae88067346bc2c6c3e99
ckong727 <54300875+ckong727@users.noreply.github.com>
2019-08-29 23:38:45 -0400

    Update A1Adept.java

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -9,6 +9,51 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		
+		double netPrice = 0;
+		int[] item = new int[scan.nextInt()];
+		String[] itemName = new String[item.length];
+		double[] itemPrice = new double[item.length];
+		for (int i = 0; i<item.length; i++) {
+			itemName[i] = scan.next();
+			itemPrice[i] = scan.nextDouble();
+		}
+		int[] customerNumber = new int[scan.nextInt()];
+		String[] firstName = new String[customerNumber.length];
+		String[] lastName = new String[customerNumber.length];
+		double[] customerTotal = new double[customerNumber.length];
+		for (int j = 0; j < customerNumber.length; j++) {
+			firstName[j] = scan.next();
+			lastName[j] = scan.next();
+			int itemCounter = scan.nextInt();
+			for (int k = 0; k < itemCounter; k++) {
+				int multiplier = scan.nextInt();
+				String testName = scan.next();
+				for (int n = 0; n < itemName.length; n++) {
+					if (itemName[n].equalsIgnoreCase(testName)) {
+					customerTotal[j] += itemPrice[n] * multiplier; 
+					}
+				}
+			}
+		}
+		scan.close();
+		int maxCustomer = 0;
+		int minCustomer = 0;
+		double comparePrice = customerTotal[0];
+		for (int p = 0; p < customerTotal.length; p++) {
+			netPrice += customerTotal[p];
+			if (customerTotal[p] > comparePrice) {
+				maxCustomer = p;
+				comparePrice = customerTotal[p];
+			}
+			else if(customerTotal[p] < comparePrice) {
+				minCustomer = p;
+				comparePrice = customerTotal[p];
+			}
+		}
+		double avg = netPrice / customerNumber.length;
+		System.out.println("Biggest: " + firstName[maxCustomer] + " " + lastName[maxCustomer] + " (" + String.format("%.2f",customerTotal[maxCustomer]) + ")" );
+		System.out.println("Smallest: " + firstName[minCustomer] + " " + lastName[minCustomer] + " (" + String.format("%.2f",customerTotal[minCustomer]) + ")" );
+		System.out.println("Average: " + String.format("%.2f", avg));
 	}
 }
+	
