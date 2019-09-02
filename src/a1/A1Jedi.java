660d845a31c38d7554ca38503b8da18519fee8a7
ChrisK <ChrisK@LAPTOP-Chris.wireless-1x.unc.edu>
2019-09-01 20:20:51 -0400

    Does it work now, Mr. Krabs?

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -9,37 +9,48 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		int[] item = new int[scan.nextInt()];
-		int[] itemCheck = new int[item.length];
-		String[] itemName = new String[item.length];
-		for (int i = 0; i<item.length; i++) {
+		int[] itemAmount = new int[scan.nextInt()];
+		int[] itemCheck = new int[itemAmount.length];
+		String[] itemName = new String[itemAmount.length];
+		for (int i = 0; i < itemName.length; i++) {
 			itemName[i] = scan.next();
-			double itemPrice = scan.nextDouble();
+			double price = scan.nextDouble();
 		}
+		int[] numCustomer = new int[scan.nextInt()];
+		String[] firstName = new String[numCustomer.length];
+		String[] lastName = new String[numCustomer.length];
 		
-		int[] customerNumber = new int [scan.nextInt()];
-		for (int j = 0; j < customerNumber.length; j++) {
-			String firstName = scan.next();
-			String lastName = scan.next();
-			int itemCounter = scan.nextInt();
-			for (int k = 0; k < itemCounter; k++) {
+		for (int i = 0; i < numCustomer.length; i++) {
+			firstName[i] = scan.next();
+			lastName[i] = scan.next();
+			int itemCount = scan.nextInt();
+			String[] individualCart = new String[itemCount];
+			for (int j = 0; j < itemCount; j++ )
+			{
 				int multiplier = scan.nextInt();
 				String testName = scan.next();
+				individualCart[j] = testName;
+				
 				for (int n = 0; n < itemName.length; n++) {
 					if (testName.equalsIgnoreCase(itemName[n])) {
-						item[n] += multiplier;
-						itemCheck[n]++;
+						itemAmount[n] += multiplier;
+							itemCheck[n]++;
+						for (int k = 0; k < j; k++) {
+							if(individualCart[k].equalsIgnoreCase(testName)) {
+						itemCheck[n]--;
+					}
+						}
 					}
 				}
+				
 			}
 		}
-		scan.close();
-		for (int p = 0;  p < item.length; p++){
+		for (int p = 0;  p < itemAmount.length; p++){
 			if (itemCheck[p] == 0) {
 				System.out.println("No customers bought " + itemName[p]);
 			}
 			else {
-				System.out.println(itemCheck[p] + " customers bought " + item[p] + " " + itemName[p]);
+				System.out.println(itemCheck[p] + " customers bought " + itemAmount[p] + " " + itemName[p]);
 			}
 		}
 	}
